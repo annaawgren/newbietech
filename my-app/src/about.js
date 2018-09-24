363df5bc31480a084a2fbf2deeae3b77fc48ef29
Anna Ågren <info@annaagren.se>
2018-09-24 16:17:54 +0200

    more fixx on about

--- my-app/src/about.js
+++ my-app/src/about.js
@@ -3,17 +3,26 @@ import React, {Component} from 'react'
 class About extends Component {
 
   render() {
-    return (<div className="about flex flex-column w-100 pl6 pr6 pt6 pb6 tl" id="about">
+    return (<div className="about flex flex-column w-100 pl6 pr6 tl" id="about">
 
-      <div className="about-container flex flex-column pa4 w-40">
+      <div className="about-container flex flex-column pl6 pa4 w-100">
 
-        <div className="team-img"></div>
+        <div className="team-img team flex self-end"></div>
 
-        <div className="about-hl mb4">About</div>
+        <div className="about-txt-container">
+          <div className="about-hl mb4">About</div>
 
-        <p>
-          Newbie tech was founded by art director/developer <a className="redishpink" href="https://www.linkedin.com/in/anna-%C3%A5gren-7abb445a/" target="blank">Anna Ågren</a>, digital designer <a className="redishpink" href="https://www.linkedin.com/in/lisaengwall/" target="blank">Lisa Engwall</a> and gamification expert <a className="redishpink" href="https://www.linkedin.com/in/frida-mons%C3%A9n-91ab0693/" target="blank">Frida Monsén</a>.
-        </p>
+          <p>
+            Newbie tech was founded by art director/developer <a className="redishpink" href="https://www.linkedin.com/in/anna-%C3%A5gren-7abb445a/" target="blank">Anna Ågren</a>, digital designer <a className="redishpink" href="https://www.linkedin.com/in/lisaengwall/" target="blank">Lisa Engwall</a> and gamification expert <a className="redishpink" href="https://www.linkedin.com/in/frida-mons%C3%A9n-91ab0693/" target="blank">Frida Monsén</a>.
+            The seed for the exploration lab was planted at Hyper Island where the duo explored creative tech together.
+            Anna and Lisa want to spread the joy and beauty of exploring and learning
+            about new technologies together with other tech newbies through facilitated,
+            high quality and curated exploration labs. Newbie tech work in the
+            intersection of creative tech and design.
+          </p>
+
+        </div>
+
 
       </div>
 
--- my-app/src/partner.js
+++ my-app/src/partner.js
@@ -1,12 +1,9 @@
 import React, {Component} from 'react'
 
-
 class Partner extends Component {
-
 
   render() {
     return (<div className="partner flex flex-column w-100 vh-100 tl justify-center items-center">
-
 
       <div className="partner-container flex flex-column justify-center items-center tc">
 
@@ -18,27 +15,16 @@ class Partner extends Component {
 
         <div className="partnerinfo">
           <p>
-            Let’s join forces! We are looking for partners
-            who share our mission to make tech fun and accessible
-            for everyone. Do not hesitate to contact us if you see
-            an opportunity to collaborate and shape Newbie tech
-            togheter with us!
+            Let’s join forces! We are looking for partners who share our mission to make tech fun and accessible for everyone. Do not hesitate to contact us if you see an opportunity to collaborate and shape Newbie tech togheter with us!
             <br/>
             <br/>
             contact <a className="redishpink" href="mailto:frida@newbietech.se">frida@newbietech.se</a>
           </p>
         </div>
 
-
-
       </div>
 
-
-
-
-
-
-      </div>)
+    </div>)
   }
 
 }
