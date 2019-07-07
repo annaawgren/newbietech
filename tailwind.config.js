83fdbbd961a5f7638c5683f6d4fa110fec8e750c
Anna Ågren <info@annaagren.se>
2019-07-07 14:30:29 +0200

    added Tailwind + configs

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  prefix: "",
+  important: false,
+  separator: ":",
+  theme: {
+    fontFamily: {
+      display: ["Open sans", "sans-serif"],
+      body: ["Open sans", "sans-serif"],
+      sans: ["Open sans", "sans-serif"]
+    },
+    extend: {
+      colors: {
+        gold: "#625441",
+        green: "#6EAB6C"
+      },
+      fontSize: {
+        mega: "12.5rem"
+      }
+    }
+  },
+  variants: {
+    appearance: ["responsive"],
+    zIndex: ["responsive"]
+  },
+  plugins: []
+};
